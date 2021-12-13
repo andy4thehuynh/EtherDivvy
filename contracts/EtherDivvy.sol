1169ba81f5d828636882acc098a1c7d3aea04753
Andy Huynh <andy4thehuynh@gmail.com>
2021-12-13 15:51:51 -0700

    ALH: Include additional testing for contract ownership

--- contracts/EtherDivvy.sol
+++ contracts/EtherDivvy.sol
@@ -4,10 +4,23 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 /*
-   Write a blurb about what your contract does
+   @dev Write a blurb about what your contract does
 */
 
 contract EtherDivvy is Ownable {
+    using SafeMath for uint;
+
+    uint public maxContribution; // maximum amount of ether an account can contribute
+
+    constructor() {
+        maxContribution = 10 ether;
+    }
+
+    function changeMaxContribution(uint _amount) public onlyOwner {
+        require(_amount > 0 ether, 'Cannot set max contribution to zero');
+        maxContribution = _amount;
+    }
 }
--- test/EtherDivvy.js
+++ test/EtherDivvy.js
@@ -2,23 +2,72 @@ const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
 describe("EtherDivvy", function() {
+  let owner;
+  let account1;
+  let account2;
+  let EtherDivvy;
+  let etherDivvy;
+
+  beforeEach(async() => {
+    [owner, account1, account2] = await ethers.getSigners();
+    EtherDivvy                  = await ethers.getContractFactory("EtherDivvy");
+    etherDivvy                  = await EtherDivvy.deploy();
+  });
+
   describe("when contract owner", function() {
     it("set to owner by default on deploying the contract", async function() {
-      const [owner]    = await ethers.getSigners();
-      const EtherDivvy = await ethers.getContractFactory("EtherDivvy");
-      const etherDivvy = await EtherDivvy.deploy();
       expect(owner.address).to.equal(await etherDivvy.owner());
     });
 
-    it("can transfer ownership to someone else", async function() {
-      const [owner, alice] = await ethers.getSigners();
-      const EtherDivvy     = await ethers.getContractFactory("EtherDivvy");
-      const etherDivvy     = await EtherDivvy.deploy();
-
-      await etherDivvy.transferOwnership(alice.address, {from: owner.address});
+    it("can transfer contractownership to someone else", async function() {
+      await etherDivvy.transferOwnership(account1.address, {from: owner.address});
 
       expect(owner.address).to.not.equal(await etherDivvy.owner());
-      expect(alice.address).to.equal(await etherDivvy.owner());
+      expect(account1.address).to.equal(await etherDivvy.owner());
+    });
+
+    it("can change max contribution", async function() {
+      const defaultMax = ethers.utils.parseEther('10');
+      const newMax     = ethers.utils.parseEther('50');
+
+      expect(await etherDivvy.maxContribution()).to.equal(defaultMax);
+
+      await etherDivvy.changeMaxContribution(newMax);
+      expect(await etherDivvy.maxContribution()).to.equal(newMax);
+    });
+
+    it("can not configure max contribution to zero ether", async function() {
+      const invalidMax = ethers.utils.parseEther('0');
+
+      await expect(etherDivvy.changeMaxContribution(invalidMax))
+        .to.be.revertedWith('Cannot set max contribution to zero');
+    });
+  });
+
+  describe("when not contract owner", function() {
+    let nonContractOwner;
+
+    beforeEach(async() => {
+      nonContractOwner = account1;
+    });
+
+    it("is not owner when calling owner function on contract", async function() {
+      expect(owner.address).to.equal(await etherDivvy.owner());
+      expect(nonContractOwner.address).to.not.equal(await etherDivvy.owner());
+    });
+
+    it("cannot transfer contract ownership to someone else", async function() {
+      await expect(
+        etherDivvy.connect(nonContractOwner)
+          .transferOwnership(owner.address, {from: nonContractOwner.address})
+        ).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+
+    it("cannot configure max contribution", async function() {
+      const newMax = ethers.utils.parseEther('5');
+
+      await expect(etherDivvy.connect(nonContractOwner).changeMaxContribution(newMax))
+        .to.be.revertedWith('Ownable: caller is not the owner');
     });
   });
 });
