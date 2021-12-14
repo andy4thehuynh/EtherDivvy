f4c2b412d7f846ec9c4451029dc740df5867219e
Andy Huynh <andy4thehuynh@gmail.com>
2021-12-14 11:50:53 -0700

    ALH: Move less important tests to bottom of file

--- test/EtherDivvy.js
+++ test/EtherDivvy.js
@@ -14,62 +14,6 @@ describe("EtherDivvy", function() {
     etherDivvy                  = await EtherDivvy.deploy();
   });
 
-  describe("when contract owner", function() {
-    it("set to owner by default on deploying the contract", async function() {
-      expect(owner.address).to.equal(await etherDivvy.owner());
-    });
-
-    it("can transfer contractownership to someone else", async function() {
-      await etherDivvy.transferOwnership(account1.address, {from: owner.address});
-
-      expect(owner.address).to.not.equal(await etherDivvy.owner());
-      expect(account1.address).to.equal(await etherDivvy.owner());
-    });
-
-    it("can change max contribution", async function() {
-      const defaultMax = ethers.utils.parseEther('10');
-      const newMax     = ethers.utils.parseEther('50');
-
-      expect(await etherDivvy.maxContribution()).to.equal(defaultMax);
-
-      await etherDivvy.changeMaxContribution(newMax);
-      expect(await etherDivvy.maxContribution()).to.equal(newMax);
-    });
-
-    it("can not configure max contribution to zero ether", async function() {
-      const invalidMax = ethers.utils.parseEther('0');
-
-      await expect(etherDivvy.changeMaxContribution(invalidMax))
-        .to.be.revertedWith('Cannot set max contribution to zero');
-    });
-  });
-
-  describe("when not contract owner", function() {
-    let nonContractOwner;
-
-    beforeEach(async() => {
-      nonContractOwner = account1;
-    });
-
-    it("is not owner when calling owner function on contract", async function() {
-      expect(owner.address).to.equal(await etherDivvy.owner());
-      expect(nonContractOwner.address).to.not.equal(await etherDivvy.owner());
-    });
-
-    it("cannot transfer contract ownership to someone else", async function() {
-      await expect(
-        etherDivvy.connect(nonContractOwner)
-          .transferOwnership(account2.address, {from: nonContractOwner.address})
-        ).to.be.revertedWith('Ownable: caller is not the owner');
-    });
-
-    it("cannot configure max contribution", async function() {
-      const newMax = ethers.utils.parseEther('5');
-
-      await expect(etherDivvy.connect(nonContractOwner).changeMaxContribution(newMax))
-        .to.be.revertedWith('Ownable: caller is not the owner');
-    });
-  });
 
   describe("when an account successfully contributes to the contract", function() {
     it("changes number of partipants by one", async function() {
@@ -130,6 +74,63 @@ describe("EtherDivvy", function() {
       });
 
       expect(await etherDivvy.numberOfPartipants()).to.equal(1);
+    });
+  });
+
+  describe("when contract owner", function() {
+    it("set to owner by default on deploying the contract", async function() {
+      expect(owner.address).to.equal(await etherDivvy.owner());
+    });
+
+    it("can transfer contractownership to someone else", async function() {
+      await etherDivvy.transferOwnership(account1.address, {from: owner.address});
+
+      expect(owner.address).to.not.equal(await etherDivvy.owner());
+      expect(account1.address).to.equal(await etherDivvy.owner());
+    });
+
+    it("can change max contribution", async function() {
+      const defaultMax = ethers.utils.parseEther('10');
+      const newMax     = ethers.utils.parseEther('50');
+
+      expect(await etherDivvy.maxContribution()).to.equal(defaultMax);
+
+      await etherDivvy.changeMaxContribution(newMax);
+      expect(await etherDivvy.maxContribution()).to.equal(newMax);
+    });
+
+    it("can not configure max contribution to zero ether", async function() {
+      const invalidMax = ethers.utils.parseEther('0');
+
+      await expect(etherDivvy.changeMaxContribution(invalidMax))
+        .to.be.revertedWith('Cannot set max contribution to zero');
+    });
+  });
+
+  describe("when not contract owner", function() {
+    let nonContractOwner;
+
+    beforeEach(async() => {
+      nonContractOwner = account1;
+    });
+
+    it("is not owner when calling owner function on contract", async function() {
+      expect(owner.address).to.equal(await etherDivvy.owner());
+      expect(nonContractOwner.address).to.not.equal(await etherDivvy.owner());
+    });
+
+    it("cannot transfer contract ownership to someone else", async function() {
+      await expect(
+        etherDivvy.connect(nonContractOwner)
+          .transferOwnership(account2.address, {from: nonContractOwner.address})
+        ).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+
+    it("cannot configure max contribution", async function() {
+      const newMax = ethers.utils.parseEther('5');
+
+      await expect(etherDivvy.connect(nonContractOwner).changeMaxContribution(newMax))
+        .to.be.revertedWith('Ownable: caller is not the owner');
     });
   });
 
