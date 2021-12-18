01df6331d0dbc3322f26dedcdd476b74a0b620d7
Andy Huynh <andy4thehuynh@gmail.com>
2021-12-18 11:51:03 -0700

    ALH: Update deploy script to Kovan

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-solhint");
+require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,6 +18,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.KOVAN_URL}`,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY]
+    }
+  }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const EtherDivvy = await hre.ethers.getContractFactory("EtherDivvy");
+  const etherDivvy = await EtherDivvy.deploy();
 
-  await greeter.deployed();
+  await etherDivvy.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("EtherDivvy live at: ", etherDivvy.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
