86a5182802f81bb0b6512cc1bf62e58b6701a8e1
Andy Huynh <andy4thehuynh@gmail.com>
2021-12-23 13:50:05 -0700

    ALH: Add missing semicolons to test helpers

--- test/helpers.js
+++ test/helpers.js
@@ -1,6 +1,6 @@
 const timeTravel = async days => {
   await ethers.provider.send('evm_increaseTime', [days * 24 * 60 * 60]);
-  await ethers.provider.send('evm_mine')
+  await ethers.provider.send('evm_mine');
 };
 
 module.exports = {
